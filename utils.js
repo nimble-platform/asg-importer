3637a9365f7c81b5d822e657de1182ed1ce6b0ab
Rohit A. Deshmukh <rohitadeshmukh13@gmail.com>
2021-11-05 16:06:08 +0100

    [ASG-IMPORTER] Added GET to the list of allowed methods for efpf_sr_admin users Users with efpf_sr_admin role configured with service_registry#sr_admin resource/scope should have full, admin-level access (CRUD) to the Service Registry API

--- utils.js
+++ utils.js
@@ -91,7 +91,7 @@ let util = {
 
 
         const srAdminBody = {
-            "methods": ["POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+            "methods": ["POST", "PUT", "PATCH", "DELETE", "GET", "OPTIONS"],
             "uri": "/apis/sr*",
             "plugins": {
                 "proxy-rewrite": {
